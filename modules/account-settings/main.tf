6b124b4348cb2b4c8015948cd3fbb16d29f45cbf
Matt Calhoun <matt@mattcalhoun.com>
2021-12-09 14:31:00 -0500

    refactor to use iam-role module

--- modules/account-settings/main.tf
+++ modules/account-settings/main.tf
@@ -1,40 +1,12 @@
 
 locals {
   create_iam_role = module.this.enabled && var.iam_role_arn == null
-  role_arn        = module.this.enabled ? var.iam_role_arn == null ? aws_iam_role.api_gateway_cloudwatch[0].arn : var.iam_role_arn : null
+  role_arn        = module.this.enabled ? var.iam_role_arn == null ? module.role.arn : var.iam_role_arn : null
 }
 
 resource "aws_api_gateway_account" "this" {
   count               = module.this.enabled ? 1 : 0
-  cloudwatch_role_arn = aws_iam_role.api_gateway_cloudwatch[0].arn
-}
-
-module "iam_role_label" {
-  source  = "cloudposse/label/null"
-  version = "0.25.0"
-
-  attributes = ["api", "gateway", "cloudwatch"]
-
-  context = module.this.context
-}
-
-resource "aws_iam_role" "api_gateway_cloudwatch" {
-  count              = local.create_iam_role ? 1 : 0
-  name               = module.iam_role_label.id
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-  tags               = module.this.tags
-}
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    sid    = "AllowAssumeRoleByAPIGW"
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["apigateway.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
+  cloudwatch_role_arn = local.role_arn
 }
 
 data "aws_iam_policy_document" "api_gateway_permissions" {
@@ -54,9 +26,26 @@ data "aws_iam_policy_document" "api_gateway_permissions" {
   }
 }
 
-resource "aws_iam_role_policy" "cloudwatch" {
-  count  = module.this.enabled ? 1 : 0
-  name   = "default"
-  role   = aws_iam_role.api_gateway_cloudwatch[0].id
-  policy = data.aws_iam_policy_document.api_gateway_permissions.json
+module "role" {
+  source  = "cloudposse/iam-role/aws"
+  version = "0.14.0"
+
+  enabled = local.create_iam_role
+  #name         = module.iam_role_label.id
+  use_fullname = true
+  attributes   = ["api", "gateway", "cloudwatch"]
+
+  principals = {
+    "Service" : ["apigateway.amazonaws.com"]
+  }
+
+  policy_documents = [
+    data.aws_iam_policy_document.api_gateway_permissions.json,
+  ]
+
+  policy_document_count = 1
+  policy_description    = "Allow API Gateway to send logs to CloudWatch IAM policy"
+  role_description      = "Allow API Gateway to send logs to CloudWatch"
+
+  context = module.this.context
 }
