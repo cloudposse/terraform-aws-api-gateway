0083b190526eee9831f0c8dda3e5f4f65e5a3b6a
Matt Calhoun <matt@mattcalhoun.com>
2021-12-09 16:36:26 -0500

    fix parallel test issue

--- test/src/common.go
+++ test/src/common.go
@@ -11,8 +11,6 @@ import (
 )
 
 func testNoChanges(t *testing.T, terraformDir string) {
-	t.Parallel()
-
 	rand.Seed(time.Now().UnixNano())
 	randID := strconv.Itoa(rand.Intn(100000))
 	attributes := []string{randID}
