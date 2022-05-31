1bc7ce508b5413c0d8c4bbccc1443b337f524951
Callum <callumccr@gmail.com>
2022-05-31 10:08:06 +0100

    fix: updated the correct rest_api_id arg

--- variables.tf
+++ variables.tf
@@ -89,7 +89,7 @@ variable "gateway_responses" {
   type = list(object({
     response_type       = string
     status_code         = string
-    response_templates  = map(string)
-    response_parameters = map(string)
+    response_templates  = map(any)
+    response_parameters = map(any)
   }))
 }
