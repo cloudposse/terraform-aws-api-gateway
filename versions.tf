25697cba85924c6f074720d5c6ad40cc6895f067
Callum <callumccr@gmail.com>
2022-08-01 14:01:20 +0100

    feat: added default backend for versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,15 @@
 terraform {
-  required_version = ">= 0.13"
-
+  experiments = [module_variable_optional_attrs]
+  backend "s3" {}
+  required_version = ">= 1.1.8"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
     }
   }
 }
