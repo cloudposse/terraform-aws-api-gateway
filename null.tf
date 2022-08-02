e554ef52d12f6a653106ab3c759a6409aace4e2f
Callum <callumccr@gmail.com>
2022-08-02 14:19:20 +0100

    feat: added default null resource

--- null.tf
+++ null.tf
@@ -1,14 +1,14 @@
-resource "aws_api_gateway_resource" "default" {
+resource "aws_api_gateway_resource" "null" {
   count       = local.enabled ? 1 : 0
   rest_api_id = aws_api_gateway_rest_api.this[0].id
   parent_id   = aws_api_gateway_rest_api.this[0].root_resource_id
   path_part   = "null"
 }
 
-resource "aws_api_gateway_method" "default" {
+resource "aws_api_gateway_method" "null" {
   count         = local.enabled ? 1 : 0
   rest_api_id   = aws_api_gateway_rest_api.this[0].id
-  resource_id   = aws_api_gateway_resource.default[0].id
+  resource_id   = aws_api_gateway_resource.null[0].id
   http_method   = "GET"
   authorization = "NONE"
 }
@@ -16,7 +16,7 @@ resource "aws_api_gateway_method" "default" {
 resource "aws_api_gateway_integration" "mock" {
   count       = local.enabled ? 1 : 0
   rest_api_id = aws_api_gateway_rest_api.this[0].id
-  resource_id = aws_api_gateway_resource.default[0].id
-  http_method = aws_api_gateway_method.default[0].http_method
+  resource_id = aws_api_gateway_resource.null[0].id
+  http_method = aws_api_gateway_method.null[0].http_method
   type        = "MOCK"
 }
