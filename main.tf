5b290e9f2b17e967e4a4bd7afe5ffb92691434a0
Callum <callumccr@gmail.com>
2022-08-02 13:58:37 +0100

    feat: added default method

--- main.tf
+++ main.tf
@@ -92,6 +92,13 @@ resource "aws_api_gateway_method_settings" "all" {
   }
 }
 
+resource "aws_api_gateway_method" "default" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_rest_api.this.root_resource_id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_model" "this" {
   for_each     = local.enabled && length(var.models) > 0 ? { for s in var.models : s.name => s } : {}
   rest_api_id  = aws_api_gateway_rest_api.this.*.id[0]
